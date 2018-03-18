a0a93f959b1e002f56f1ada00a6828d6d1509b35
Muhammad Fadhlika <fadhlika@gmail.com>
2018-03-18 13:27:26 +0700

    add mqtt

--- Weather-Station-Base.ino
+++ Weather-Station-Base.ino
@@ -2,11 +2,15 @@
 #include <LoRa.h>
 #include <WiFi.h>
 #include <ArduinoJson.h>
+#include <PubSubClient.h>
 
 const char* proxy_server = "167.205.22.103";
 const unsigned int proxy_port = 8080;
 const char* server = "api.aviana.fadhlika.com";
 const unsigned int port = 80;
+
+const char* mqttserver = "fadhlika.com";
+const unsigned int mqttport = 1883;
 
 const String nodeid[] = {"baafm7rmke00rou2k8ig" , "baaeum3mke00e9u4pdtg"};
 const String fields[] = {"date", "dir", "voltage", "speed", "rainfall"};
@@ -16,11 +20,18 @@ const char* password = "12345678!";
 
 char * data;
 char * cmd;
+char * devstat;
 bool isdata = false;
 bool iscmd = false;
 
 int pkt;
 int rssi;
+
+#define TIMEOUT 0x02
+#define BAD 0x03;
+
+WiFiClient client;
+PubSubClient mqttclient(client);
 
 void setup() {
   Serial.begin(115200);
@@ -43,8 +54,11 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  Serial.println("LoRa Sender");
+  mqttclient.setServer(mqttserver, mqttport);
+  mqttclient.setCallback(mqttcallback);
 
+  Serial.println("Weather Station");
+  
   LoRa.setPins(5, 17, 22);
   if (!LoRa.begin(433E6)) {
     Serial.println("Starting LoRa failed!");
@@ -59,6 +73,21 @@ void setup() {
 }
 
 void loop() {
+  if(!mqttclient.connected())
+  {
+    if(mqttclient.connect("aviana"))
+    {
+      Serial.println("MQTT connected");
+      mqttclient.publish("aviana/status", "connected");
+      mqttclient.subscribe("aviana/cmd");
+      mqttclient.subscribe("aviana/check");
+    } else {
+      Serial.println("MQTT failed, rc=");
+      Serial.println(mqttclient.state());
+    }
+  }
+  mqttclient.loop();
+  
   if(Serial.available() > 0) {
     int i = 0;
     cmd = new char[10];
@@ -68,22 +97,27 @@ void loop() {
     }
 
     pkt = i;
+    iscmd = true;
   }
 
   if(iscmd) {
-    LoRa.beginPacket();
-    for(int j=0; j < pkt; j++) {
-      LoRa.write(cmd[j]);
-      Serial.print(cmd[j], HEX);
-      Serial.print(" ");
-    }
-    LoRa.endPacket();
-    Serial.println();
-    LoRa.receive();
+    mqttclient.publish("aviana/status", devstat);
+    if(pkt > 0) {
+      LoRa.beginPacket();
+      for(int j=0; j < pkt; j++) {
+        LoRa.write(cmd[j]);
+        Serial.print(cmd[j], HEX);
+        Serial.print(" ");
+      }
+      LoRa.endPacket();
+      Serial.println(); 
+      LoRa.receive();
 
-    delete[] cmd;
+      delete[] cmd;
+      delete[] devstat;
+      pkt = 0;
+    }
     iscmd = false;
-    pkt = 0;
   }
 
   if(isdata) {
@@ -117,7 +151,6 @@ void loop() {
     isdata = false;
     delete[] data;
 
-    WiFiClient client;
     if (client.connect(proxy_server, proxy_port)) {
         
         String req = "POST http://" + String(server) + "/data?id=baaeum3mke00e9u4pdtg HTTP/1.1\r\n";
@@ -130,20 +163,34 @@ void loop() {
         client.println();
     }
 
+    char * msg;
     unsigned long timeout = millis();
     while (client.available() == 0) {
         if (millis() - timeout > 1000) {
             Serial.println("»> Client Timeout !");
+            msg =  new char[3]{ '1', ';', TIMEOUT};
             client.stop();
             return;
         }
     }
     // Read all the lines of the reply from server and print them to Serial
-    while(client.available()) {
-        String line = client.readStringUntil('\r');
-        Serial.print(line);
+    String line;
+    if(client.available()) {
+        line = client.readStringUntil('\r');
+        msg = new char[line.length() + 1];
+        strcpy(msg, line.c_str());
     }
     client.stop();
+    
+    if(!mqttclient.connected()){
+      if(mqttclient.connect("aviana")){
+        mqttclient.subscribe("aviana/cmd");
+        mqttclient.subscribe("aviana/check");
+      }
+    }
+
+    mqttclient.publish("aviana/status", msg);    
+    delete[] msg;
   }
 }
 
@@ -153,9 +200,13 @@ void onReceive(int packetSize) {
     // received a packet
     Serial.print("Received packet '");
 
-    for (int i = 0; i < packetSize; i++) {
-      Serial.print((char)LoRa.read());
+    devstat = new char[packetSize + 1];
+    int i;
+    for (i = 0; i < packetSize; i++) {
+      devstat[i] = LoRa.read();
     }
+    devstat[i] = '\0';
+    Serial.print(devstat);
     // print RSSI of packet
     Serial.print("' with RSSI ");
     Serial.println(LoRa.packetRssi());
@@ -172,3 +223,25 @@ void onReceive(int packetSize) {
     isdata = true;
   }
 }
+
+void mqttcallback(char* topic, byte* payload, unsigned int length)
+{
+  Serial.print("MQTT: [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  if(strcmp(topic, "aviana/check") == 0)
+  {
+    Serial.println("check");
+    mqttclient.publish("aviana/status", "alive");
+    return;
+  }
+
+  cmd = new char[length];
+  for(int i=0; i<length; i++)
+  {
+    cmd[i] = payload[i] - '0';
+  }
+  pkt = length;
+}
+
